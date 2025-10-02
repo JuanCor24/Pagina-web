cc7aea10c02caecb2ad8638420e8fcc6610aedfc
JuanCor24 <74743148+JuanCor24@users.noreply.github.com>
2025-10-02 23:57:23 +0200

    Cambios

---
+++ movimiento.js
@@ -0,0 +1,11 @@
+let menuVisible = false;
+function mostrarOcultarMenu{
+    if(menuVisible){
+        document.getElementById("nav").classList = "";
+        menuVisible = false;
+    }
+    else{
+        document.getElementById("nav").classList = "responsive";
+        menuVisible = true;
+    }
+}
